48c096bf97c98afb2091107723798a79fa2b4cf9
Rakesh kumar <rakesh.cse12@gmail.com>
2016-05-10 11:29:54 +0530

    fixed css issues

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,7 +2,15 @@
 
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
-
+Rails.application.config.assets.precompile += %w( bootstrap.min.css )
+Rails.application.config.assets.precompile += %w( main-style.css )
+Rails.application.config.assets.precompile += %w( fonts/font-awesome/css/font-awesome.min.css )
+Rails.application.config.assets.precompile += %w( 320.css )
+Rails.application.config.assets.precompile += %w( 480.css )
+Rails.application.config.assets.precompile += %w( 640.css )
+Rails.application.config.assets.precompile += %w( 768.css )
+Rails.application.config.assets.precompile += %w( 992.css )
+Rails.application.config.assets.precompile += %w( 1200.css )
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
