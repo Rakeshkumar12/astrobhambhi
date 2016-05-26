cbc4e6016391bfe9d7a21ce66a42a574065135b3
roivnv-chetu <roivnv@chetu.com>
2016-05-26 23:25:42 +0530

    added active admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  # def ccavenue
+  #   return @ccavenue = Ccavenue::Payment.new(<TODO: MERCHANT ID>,<TODO: MERCHANT WORKING KEY>,<TODO: REDIRECT URL>)
+  # end
   protected
 
   def configure_permitted_parameters
