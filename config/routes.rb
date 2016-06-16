e7c6e930a01fb13ef283ee290251c8e050f24d22
Rakesh kumar <rakesh.cse12@gmail.com>
2016-06-16 13:30:17 +0530

    route

--- config/routes.rb
+++ config/routes.rb
@@ -8,12 +8,18 @@ Rails.application.routes.draw do
 
   devise_for :users
   root 'home#index'
-  get 'home/index'
+  #get 'home/index'
   post "/orders/:id" => "orders#show"
   post "/hook" => "orders#hook"
   get '/orders/pay_amount' => "orders#pay_amount"
   get 'orders/ccavResponseHandler' => "orders#ccavResponseHandler"
-  root 'home#index'
+  #root 'home#index'
+  get 'home/astroservices'
+  get 'home/vastu'
+  get 'home/numerology'
+  get 'home/shop'
+  get 'home/horoscope'
+  get 'home/contact'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
